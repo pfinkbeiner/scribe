ddc9b5f46bdd57695994098b122fa62c9e6e3ba1
Patrick Finkbeiner <finkbeiner.patrick@gmail.com>
2018-04-05 12:17:04 +0200

    Skip paste hook on IE11.

--- src/plugins/core/events.js
+++ src/plugins/core/events.js
@@ -180,68 +180,74 @@ define([
        * I also don't like how it has the authority to perform `event.preventDefault`.
        */
 
-      scribe.el.addEventListener('paste', function handlePaste(event) {
-        /**
-         * Browsers without the Clipboard API (specifically `ClipboardEvent.clipboardData`)
-         * will execute the second branch here.
-         *
-         * Chrome on android provides `ClipboardEvent.clipboardData` but the types array is not filled
-         */
-        if (event.clipboardData && event.clipboardData.types.length > 0) {
-          event.preventDefault();
-
-          if (Immutable.List(event.clipboardData.types).includes('text/html')) {
-            scribe.insertHTML(event.clipboardData.getData('text/html'));
-          } else {
-            scribe.insertPlainText(event.clipboardData.getData('text/plain'));
-          }
-        } else {
+      /**
+       * Looks like placeMarker doens't work as expected on IE11.
+       * Therefore we skip the entire paste hook
+       */
+      var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+      if (!isIE11) {
+        scribe.el.addEventListener('paste', function handlePaste(event) {
           /**
-           * If the browser doesn't have `ClipboardEvent.clipboardData`, we run through a
-           * sequence of events:
+           * Browsers without the Clipboard API (specifically `ClipboardEvent.clipboardData`)
+           * will execute the second branch here.
            *
-           *   - Save the text selection
-           *   - Focus another, hidden textarea so we paste there
-           *   - Copy the pasted content of said textarea
-           *   - Give focus back to the scribe
-           *   - Restore the text selection
-           *
-           * This is required because, without access to the Clipboard API, there is literally
-           * no other way to manipulate content on paste.
-           * As per: https://github.com/jejacks0n/mercury/issues/23#issuecomment-2308347
-           *
-           * Firefox <= 21
-           * https://developer.mozilla.org/en-US/docs/Web/API/ClipboardEvent.clipboardData
+           * Chrome on android provides `ClipboardEvent.clipboardData` but the types array is not filled
            */
+          if (event.clipboardData && event.clipboardData.types.length > 0) {
+            event.preventDefault();
 
-          var selection = new scribe.api.Selection();
-
-          // Store the caret position
-          selection.placeMarkers();
-
-          var bin = document.createElement('div');
-          document.body.appendChild(bin);
-          bin.setAttribute('contenteditable', true);
-          bin.focus();
-
-          // Wait for the paste to happen (next loop?)
-          setTimeout(function () {
-            var data = bin.innerHTML;
-            bin.parentNode.removeChild(bin);
-
-            // Restore the caret position
-            selection.selectMarkers();
+            if (Immutable.List(event.clipboardData.types).includes('text/html')) {
+              scribe.insertHTML(event.clipboardData.getData('text/html'));
+            } else {
+              scribe.insertPlainText(event.clipboardData.getData('text/plain'));
+            }
+          } else {
             /**
-             * Firefox 19 (and maybe others): even though the applied range
-             * exists within the Scribe instance, we need to focus it.
+             * If the browser doesn't have `ClipboardEvent.clipboardData`, we run through a
+             * sequence of events:
+             *
+             *   - Save the text selection
+             *   - Focus another, hidden textarea so we paste there
+             *   - Copy the pasted content of said textarea
+             *   - Give focus back to the scribe
+             *   - Restore the text selection
+             *
+             * This is required because, without access to the Clipboard API, there is literally
+             * no other way to manipulate content on paste.
+             * As per: https://github.com/jejacks0n/mercury/issues/23#issuecomment-2308347
+             *
+             * Firefox <= 21
+             * https://developer.mozilla.org/en-US/docs/Web/API/ClipboardEvent.clipboardData
              */
-            scribe.el.focus();
 
-            scribe.insertHTML(data);
-          }, 1);
-        }
-      });
+            var selection = new scribe.api.Selection();
 
+            // Store the caret position
+            selection.placeMarkers();
+
+            var bin = document.createElement('div');
+            document.body.appendChild(bin);
+            bin.setAttribute('contenteditable', true);
+            bin.focus();
+
+            // Wait for the paste to happen (next loop?)
+            setTimeout(function () {
+              var data = bin.innerHTML;
+              bin.parentNode.removeChild(bin);
+
+              // Restore the caret position
+              selection.selectMarkers();
+              /**
+               * Firefox 19 (and maybe others): even though the applied range
+               * exists within the Scribe instance, we need to focus it.
+               */
+              scribe.el.focus();
+
+              scribe.insertHTML(data);
+            }, 1);
+          }
+        });
+      }
     };
   };
 });
